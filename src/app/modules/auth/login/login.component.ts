7593c1443b589847ada57b20143f0144898f9de3
hemraj.labaniya <hemraj.labaniya@rightwatts.com>
2020-04-03 10:39:28 +0530

    fixed issue

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {AuthService} from '../auth.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 import { NgForm } from '@angular/forms';
-import {enquiry} from '../../services/enquiry';
+import { enquiry } from '../../services/enquiry';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FetchSideNavService } from '../../services/fetch-side-nav.service';
 
@@ -16,51 +16,32 @@ declare var $;
 export class LoginComponent implements OnInit, OnDestroy {
 
   loginCred: any;
-  
-  
-  constructor(private _authservice: AuthService, private router: Router, private _loginService :FetchSideNavService) {
+
+
+  constructor(private _authservice: AuthService, private router: Router, private _loginService: FetchSideNavService) {
   }
 
   ngOnInit() {
-    // $('body').addClass('hold-transition login-page');
-    // $(() => {
-    //   $('input').iCheck({
-    //     checkboxClass: 'icheckbox_square-blue',
-    //     radioClass: 'iradio_square-blue',
-    //     increaseArea: '20%' /* optional */
-    //   });
-    // });
-   
   }
 
   ngOnDestroy(): void {
-    // $('body').removeClass('hold-transition login-page');
   }
 
-  login(){
-    // console.log("ok");
-    // localStorage.setItem('key', 'test');
-    // this._authservice.login(); 
-
-    
-    this.router.navigate(['/members']);  
+  login() {
+    this.router.navigate(['/members']);
   }
 
-  submitCredentials(loginForm: NgForm){
-
+  submitCredentials(loginForm: NgForm) {
     this.loginCred = new enquiry(loginForm.value['Username'], loginForm.value['password']);
-
     this._loginService.loginStatus(this.loginCred)
-          .subscribe(
-            data => {
-              loginForm.reset();
-            },
-            error => {
-              console.log("error", error)
-            }
-          )
-
-
+      .subscribe(
+        data => {
+          loginForm.reset();
+        },
+        error => {
+          console.log("error", error)
+        }
+      )
   }
 
 }
